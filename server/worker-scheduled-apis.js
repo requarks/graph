968d79eb96e6e0f0db5e177c286258b8046d822a
Nicolas Giard <github@ngpixel.com>
2018-09-23 19:39:13 -0400

    fix: refresh localizations frequently

--- server/worker-scheduled-apis.js
+++ server/worker-scheduled-apis.js
@@ -23,8 +23,20 @@ let jobHourly = new CronJob({
   onTick () {
     return Promise.each([
       () => { return GR.models.location.refreshCountries() },
-      () => { return GR.models.localization.refreshLocales() },
       () => { GR.logger.debug('All scheduled [APIs][Hourly] tasks completed.') }
+    ], fn => fn())
+  },
+  runOnInit: GR.conf.cron.execOnInit,
+  start: false,
+  timeZone: 'America/New_York'
+})
+
+let jobFrequent = new CronJob({
+  cronTime: GR.conf.cron.frequent,
+  onTick () {
+    return Promise.each([
+      () => { return GR.models.localization.refreshLocales() },
+      () => { GR.logger.debug('All scheduled [APIs][Frequent] tasks completed.') }
     ], fn => fn())
   },
   runOnInit: GR.conf.cron.execOnInit,
@@ -34,5 +46,6 @@ let jobHourly = new CronJob({
 
 _.delay(() => {
   jobHourly.start()
+  jobFrequent.start()
   GR.logger.info('Worker {scheduled-apis} state: [ RUNNING ]')
 }, 1000)
