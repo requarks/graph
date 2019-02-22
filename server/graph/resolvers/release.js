1194c57602e43dd9492ef54a5b60baf6540d1182
Nick <github@ngpixel.com>
2019-02-22 16:51:45 -0500

    fix: current beta release

--- server/graph/resolvers/release.js
+++ server/graph/resolvers/release.js
@@ -13,8 +13,8 @@ module.exports = {
       // TODO
       return {
         channel: 'BETA',
-        version: '2.0.0-beta.11',
-        releaseDate: moment.utc('2019-01-20T08:00:00.000Z'),
+        version: '2.0.0-beta.42',
+        releaseDate: moment.utc('2019-02-17T08:00:00.000Z'),
         minimumVersionRequired: '2.0.0-beta.0',
         minimumNodeRequired: '10.12.0'
       }
