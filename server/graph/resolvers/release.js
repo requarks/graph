640066c21b5baed6ab969d25d2a56bd0c3fa70e0
Nicolas Giard <github@ngpixel.com>
2019-03-24 18:59:01 -0400

    misc: update BETA release

--- server/graph/resolvers/release.js
+++ server/graph/resolvers/release.js
@@ -13,7 +13,7 @@ module.exports = {
       // TODO
       return {
         channel: 'BETA',
-        version: '2.0.0-beta.68',
+        version: '2.0.0-beta.84',
         releaseDate: moment.utc('2019-03-17T08:00:00.000Z'),
         minimumVersionRequired: '2.0.0-beta.0',
         minimumNodeRequired: '10.12.0'
