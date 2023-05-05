d7422c0ccd7a58af9c92a2ff4bd9096ab5f57b0e
Nicolas Giard <github@ngpixel.com>
2023-05-05 16:48:13 -0400

    fix: Update open-collective.js

--- server/services/open-collective.js
+++ server/services/open-collective.js
@@ -11,7 +11,21 @@ module.exports = {
   }),
   async getBackers () {
     try {
-      const backers = await this.http.get()
+      let hasMoreBackers = false
+      let offset = 0
+      const backers = []
+      do {
+        const resp = await this.http.get({
+          qs: {
+            offset
+          },
+          json: true,
+          resolveWithFullResponse: true
+        })
+        backers.push(...resp.body)
+        hasMoreBackers = resp.body.length > 0
+        offset += 100
+      } while (hasMoreBackers)
       return _.filter(backers || [], ['role', 'BACKER'])
     } catch (err) {
       throw new Error(err.message)
